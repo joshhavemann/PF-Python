4ad97372d06ab62c493033e3f2162086ebf234d0
Josh Havemann <104509556+joshhavemann@users.noreply.github.com>
2022-07-29 17:13:21 +0100

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -49,6 +49,7 @@ but this is included as a yellow (plus red) for European games.
 '''
 import numpy as np
 import pandas as pd
+from error import actual
 
 # Sets of coefficients used for minimisation
 co_FTHG = 1
@@ -68,8 +69,8 @@ co_AY = 1
 co_HR = 1
 co_AR = 1
 
-hc = np.zeros(16)+1
-ac = np.zeros(16)+1
+hc = np.zeros(16)+1/16
+ac = np.zeros(16)+1/16
 
 def singleEntry(csv, homeTeam, awayTeam, homeCoeffs, awayCoeffs):
     '''
@@ -100,9 +101,92 @@ def singleEntry(csv, homeTeam, awayTeam, homeCoeffs, awayCoeffs):
         awayValue += awayCoeffs[i] * df.iloc[0][i]
     return homeValue, awayValue
 
-def matrix(csv, pastCSV, useLast):
-    matrix = np.zeros(shape=(20,20))
-    return matrix
+def getCSV(season):
+    '''
+    Single csv given string of season
+    '''
+    return "https://www.football-data.co.uk/mmz4281/"+season+"/E0.csv"
+
+def getCSVs(currentSeason,league):
+    '''
+    Will return a CSV for that season and league
+    (0 for prem, 1 for championship), and the past 3 seasons before that,
+    needs to be strings in the format '2122' (for example).
+    '''
+    part1 = "https://www.football-data.co.uk/mmz4281/"
+    part2 = "/E"
+    part3 = ".csv"
+    int1 = int(currentSeason[0:2])
+    int2 = int(currentSeason[2:4])
+    csv1 = part1+currentSeason+part2+league+part3
+    csv2 = part1+str(int1-1)+str(int2-1)+part2+league+part3
+    csv3 = part1+str(int1-2)+str(int2-2)+part2+league+part3
+    csv4 = part1+str(int1-3)+str(int2-3)+part2+league+part3
+    return csv1, csv2, csv3, csv4
+
+seasonWeighting = 2/3
+minus1Weighting = 1/2
+minus2Weighting = 1/4
+minus3Weighting = 1/8
+
+def pastPrep(lastseason):
+    csvA, csvB, csvC, csvD = getCSVs(lastseason,"0")
+    # 2021 1920 1819 1718
+    csvE, csvF, csvG, csvH = getCSVs(lastseason,"1")
+    data = pd.read_csv(csvA,encoding = 'unicode_escape')
+    print(data.HomeTeam.unique())
+    last = actual(pd.read_csv(csvE,encoding = 'unicode_escape'))
+    lastprem = actual(pd.read_csv(csvA,encoding = 'unicode_escape'))
+    ch1 = last[0,0]
+    ch2 = last[1,0]
+    ch3 = last[2,0]
+    pr18 = lastprem[17,0]
+    pr19 = lastprem[18,0]
+    pr20 = lastprem[19,0]
+    data.replace(pr18, ch1, inplace = True)
+    data.replace(pr19, ch2, inplace = True)
+    data.replace(pr20, ch3, inplace = True)
+    print(data.HomeTeam.unique())
+    return data
+
+
+
+def matrix(currentSeason):
     
+    csvA, csvB, csvC, csvD = getCSVs("2122","0")
+    df = pd.read_csv(csvA,encoding = 'unicode_escape')
+    names = df.HomeTeam.unique()
+    teams = len(names)
+    seasonForm = np.zeros(teams)
+    matrix = np.zeros(shape=(teams,teams))
+    print(names)
+    entries = len(df)
+    played = np.eye(teams)
+    homeSF = 1
+    awaySF = 1
+    for k in range(entries): # Extracting data from dataframe
+        home = df['HomeTeam'][k]
+        away = df['AwayTeam'][k]
+        index1 = np.argwhere(names == home)[0][0]
+        index2 = np.argwhere(names == away)[0][0]
+        played[index1,index2] = 1
 
+    for i in range(teams):
+        for j in range(teams):
+            if played[i,j] == 1:
+                homescore, awayscore = singleEntry(csvA,names[i],names[j],hc,ac)
+                seasonForm[i] += homescore
+                seasonForm[j] += awayscore
 
+            elif played[j,i] == 1:
+                homescore, awayscore = singleEntry(csvA,names[j],names[i],hc,ac)
+                matrix[i,j] += homeSF * homescore
+                matrix[j,i] += awaySF * awayscore
+
+            else:
+                # turn past prep to csv and input
+                homescore, awayscore = singleEntry(csvB,names[i],names[j],hc,ac)
+                matrix[i,j] += homescore
+                matrix[j,i] += awayscore
+            #then add guaranteed past results and season form then add actual results in, then minimise error, and scale by a constant factor (then adding in results again unscaled)
+    return matrix
